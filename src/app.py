43e61198ace45b9d1e422eac5d2280e685a04976
fernandaluft <108574888+fernandaluft@users.noreply.github.com>
2024-01-20 13:50:10 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -24,7 +24,7 @@ def main():
         result = predict_spam(review)
         st.write(f"Classification: {result}")
 
-#if __name__ == "__main__":
-#   main()
+if __name__ == "__main__":
+   main()
 
 
